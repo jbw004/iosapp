22e35fc77aa40e4a033e2790252ae6ac20a00a39
jbw004 <jeffreybweldon@gmail.com>
2024-11-24 17:07:08 -0800

    Initial Commit

---
+++ Tell Me A Story/Tell_Me_A_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tell_Me_A_StoryApp.swift
+//  Tell Me A Story
+//
+//  Created by Jeffrey Weldon on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Tell_Me_A_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
